5f6345e9019287cf32b28dc8997677f9a23a2b9d
stsoy01 <tsoy@perco.ru>
2024-08-08 19:27:19 +0300

    1base

---
+++ homeworks/Base/base1.js
@@ -0,0 +1,21 @@
+/**
+ * Написать функцию, которая принимает Uint8Array и позволяет обратиться к биту конкретного элемента
+ * const bitGetter = createBitGetter(new Uint8Array([0b1110, 0b1101]));
+ *
+ * // Второй параметр это порядок бита "справа-налево"
+ * console.log(bitGetter.get(0, 1)); // 1
+ * console.log(bitGetter.get(1, 1)); // 0
+ */
+
+
+const createBitGetter = (object) => {
+    return {
+        get: function (index1, index2) {
+            return object[index1]
+        }
+    }
+}
+
+const bigGetter = createBitGetter(new Uint8Array([0b1110, 0b1101]))
+console.log(bigGetter.get(0, 1)) //1
+console.log(bigGetter.get(1, 1)) //0
