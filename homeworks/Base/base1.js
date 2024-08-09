8bf860a3c4f105042db53dfef0cdb4f3054a7731
stsoy01 <tsoy@perco.ru>
2024-08-09 13:07:28 +0300

    upd

--- homeworks/Base/base1.js
+++ homeworks/Base/base1.js
@@ -7,17 +7,14 @@
  * console.log(bitGetter.get(1, 1)); // 0
  */
 
-//https://amedovbekmuhamet.netlify.app/blog/intro-array-buffer
-
-
 const createBitGetter = (object) => {
     return {
         get: function (index1, index2) {
-            return object[index1]
+            return (object[index1]).toString(2).at(-1-index2)
         }
     }
 }
 
 const bigGetter = createBitGetter(new Uint8Array([0b1110, 0b1101]))
-console.log(bigGetter.get(0, 1)) //1
-console.log(bigGetter.get(1, 1)) //0
+console.log(bigGetter.get(0, 1)) // 1110 ----> 1
+console.log(bigGetter.get(1, 1)) // 1101 ----> 0
