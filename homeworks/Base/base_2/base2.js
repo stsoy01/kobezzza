84ddd9bcf2b7bf345bfb6aaa9ade124bea5a8f14
stsoy01 <tsoy@perco.ru>
2024-08-14 18:55:30 +0300

    upd

--- homeworks/Base/base_2/base2.js
+++ homeworks/Base/base_2/base2.js
@@ -1,16 +1,36 @@
 const c = document.getElementById('canvas');
 const ctx = c.getContext('2d');
 
-
 const img = new Image();
 img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuz93Fxf9lCbMrc_nVURGpjZkrxxX8ag1YDA&s'
 img.crossOrigin = "Anonymous";
-img.height = 270
 
 img.onload = () => {
     ctx.drawImage(img, 0, 0);
     const imgData = ctx.getImageData(0, 0, c.width, c.height);
-    console.log(imgData.data)
+
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] = 255 - imgData.data[i];
+        imgData.data[i + 1] = 255 - imgData.data[i + 1];
+        imgData.data[i + 2] = 255 - imgData.data[i + 2];
+        imgData.data[i + 3] = 255;
+    }
+
+    setTimeout(() => {
+        ctx.putImageData(imgData, 0, 0);
+    }, 2000)
+}
+
+function imageInitialization() {
+
+}
+
+function grayscale() {
+
+}
+
+function inverse() {
+
 }
 
 
