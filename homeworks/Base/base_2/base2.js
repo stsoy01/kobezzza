9a33ef6f8602b588770cc6c7c0ed680252a34dd1
stsoy01 <tsoy@perco.ru>
2024-08-14 17:34:04 +0300

    upd

--- homeworks/Base/base_2/base2.js
+++ homeworks/Base/base_2/base2.js
@@ -1,14 +1,13 @@
-const c = document.getElementById('canvas')
-
+const c = document.getElementById('canvas');
 const ctx = c.getContext('2d');
-const img = new Image(600, 300)
+
+
+const img = new Image();
 img.src = './hanako.png'
-
-
-ctx.drawImage(img, 0, 0);
+img.onload = () => {
+    ctx.drawImage(img, 0, 0);
+}
 
 
 const imgData = ctx.getImageData(0, 0, c.width, c.height);
-ctx.putImageData(img, 0, 0)
-
-
+console.log(imgData)
