4e5222018f1bcec8adddb85e0481914bf4e0db72
stsoy01 <tsoy@perco.ru>
2024-08-14 18:39:47 +0300

    upd

--- homeworks/Base/base_2/base2.js
+++ homeworks/Base/base_2/base2.js
@@ -3,14 +3,15 @@ const ctx = c.getContext('2d');
 
 
 const img = new Image();
-img.src = './hanako.png'
+img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuz93Fxf9lCbMrc_nVURGpjZkrxxX8ag1YDA&s'
+img.crossOrigin = "Anonymous";
+img.height = 270
+
 img.onload = () => {
     ctx.drawImage(img, 0, 0);
+    const imgData = ctx.getImageData(0, 0, c.width, c.height);
+    console.log(imgData.data)
 }
-
-
-const imgData = ctx.getImageData(0, 0, c.width, c.height);
-console.log(imgData)
 
 
 // const grayscaled = grayscale('/myImage.jpeg');
