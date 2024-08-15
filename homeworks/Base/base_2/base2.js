7941603b502fc644a87cee29dc1a6e71e358f152
stsoy01 <stsoy01@gmail.com>
2024-08-15 20:53:17 +0300

    upd

--- homeworks/Base/base_2/base2.js
+++ homeworks/Base/base_2/base2.js
@@ -1,5 +1,17 @@
+// ## Написать фильтры для изображения в Canvas
+
+// Необходимо написать функции, которые бы принимали ссылку на изображение или canvas и применяла бы к нему один из эффектов.
+// Например, инверсия цветов или оттенки серого. Для реализации эффектов, необходимо использовать методы Canvas getImageData/putImageData
+// и работа с цветами пикселей. Возвращать такая функция может ссылку на Canvas или ImageData.
+
+// const grayscaled = grayscale('/myImage.jpeg');
+// const inversed = inverse(grayscaled);
+
+/**
+
 const btn_inverse = document.getElementById('inverse')
 const btn_grayscale = document.getElementById('grayscale')
+const btn_reset = document.getElementById('reset')
 
 const c = document.getElementById('canvas');
 const ctx = c.getContext('2d');
@@ -9,11 +21,18 @@ const img = new Image();
 img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuz93Fxf9lCbMrc_nVURGpjZkrxxX8ag1YDA&s';
 img.crossOrigin = "Anonymous";
 
-img.onload = () => {
-  ctx.drawImage(img, 0, 0);
-  imgData = ctx.getImageData(0, 0, c.width, c.height);
-}
 
+btn_reset.addEventListener('click', initialization)
+function initialization() {
+  img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuz93Fxf9lCbMrc_nVURGpjZkrxxX8ag1YDA&s';
+  img.crossOrigin = "Anonymous";
+
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0);
+    imgData = ctx.getImageData(0, 0, c.width, c.height);
+  }
+}
+initialization()
 
 
 btn_inverse.addEventListener('click', inverse)
@@ -40,7 +59,75 @@ function grayscale() {
 
   ctx.putImageData(imgData, 0, 0);
 }
-// const grayscaled = grayscale('/myImage.jpeg');
-// const inversed = inverse(grayscaled);
 
-// https://inspirnathan.com/posts/105-fix-tainted-canvas-from-cross-origin-error
+ */
+
+
+
+
+
+
+// Написать функцию кодирования информации по схеме
+
+
+// const schema = [
+//   [3, 'number'],  // 3 бита число
+//   [2, 'number'],  // 2 бита число
+//   [1, 'boolean'], // 1 бит логический
+//   [1, 'boolean'], // 1 бит логический
+//   [16, 'ascii']  // 16 бит 2 аски символа
+// ];
+
+// Если данные не подходят схеме - выбрасывать исключения с пояснением.
+// Результат - ArrayBuffer.
+// const data = encode([2, 3, true, false, 'ab'], schema);
+
+const schema = [
+  [3, 'number'],  // 3 бита число
+  [2, 'number'],  // 2 бита число
+  [1, 'boolean'], // 1 бит логический
+  [1, 'boolean'], // 1 бит логический
+  [16, 'ascii']  // 16 бит 2 аски символа
+];
+
+function encode(data, schema) {
+  let encodedArray = [];
+
+  for (let i = 0; i < schema.length; i++) {
+    switch (schema[i][1]) {
+      case 'number':
+        encodedArray.push(encodeInteger(schema[i][0], data[i]));
+        break;
+      case 'boolean':
+        encodedArray.push(encodeBoolean(data[i]));
+        break;
+      case 'ascii':
+        encodedArray.push(encodeAscii(data[i]));
+        break;
+    }
+  }
+  console.log(encodedArray);
+}
+
+const data = encode([2, 3, true, false, 'ab'], schema);
+
+function encodeInteger(biteSizeController, dataValue) {
+  if (biteSizeController >= dataValue.toString(2).length) return dataValue.toString(2)
+
+  throw new Error(`Validation schema Error: length of integers bits is greater than ${biteSizeController}`);
+}
+
+function encodeBoolean(dataValue) {
+  return (+dataValue).toString(2);
+}
+
+function encodeAscii(asciiChars) {
+  let charBitesArray = [];
+  for (const char of asciiChars) {
+    charBitesArray.push((char.charCodeAt().toString(2)))
+  }
+  const charBites = charBitesArray.join('');
+  if (charBites.length <= 16) return charBites;
+
+  throw new Error('Validation schema Error: Bits length is greater than 16 bits')
+}
