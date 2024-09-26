591c057872e957c583cb732c2339af4cc8486713
stsoy01 <stsoy01@gmail.com>
2024-09-26 16:09:04 +0300

    upd

--- homeworks/Base/base_3/base3.js
+++ homeworks/Base/base_3/base3.js
@@ -15,37 +15,37 @@
 
 class LinkedNode {
 
-    value;
-    next
+  value;
+  next
 
-    constructor(value, next = null) {
-        this.value = value;
-        this.next = next;
-    }
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 class LinkedList {
 
-    first;
-    last;
+  first;
+  last;
 
-    constructor() {
-        this.first = null;
-        this.last = null;
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  add(value) {
+    const node = new LinkedNode(value);
+
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+      return;
     }
 
-    add(value) {
-        const node = new LinkedNode(value);
-
-        if (!this.first) {
-            this.first = node;
-            this.last = node;
-            return;
-        }
-
-        this.last.next = node;
-        this.last = node;
-    }
+    this.last.next = node;
+    this.last = node;
+  }
 }
 
 const list = new LinkedList();
@@ -53,7 +53,7 @@ list.add(1);
 list.add(2);
 list.add(3);
 
-console.log(list)
+// console.log(list)
 
 /**
  * Сделать связанный список итерируемым *
@@ -67,3 +67,60 @@ console.log(list)
  *   console.log(value);
  * }
  */
+
+
+/**
+ *    ```js
+   const jackBlack = Structure([
+     ['name', 'utf16', 10], // Число - это максимальное количество символов
+     ['lastName', 'utf16', 10],
+     ['age', 'u16'] // uint16
+   ]);
+   
+   jackBlack.set('name', 'Jack');
+   jackBlack.set('lastName', 'Black');
+   jackBlack.set('age', 53);
+   
+   console.log(jackBlack.get('name')); // 'Jack'
+   ```
+
+ */
+
+const Structure = (arr) => {
+  return {
+    i: 0,
+    obj: {},
+
+    set: function (key, value) {
+      if (!arr[this.i][2] && value.length > arr[this.i][2]) return;
+
+      let BYTES_PER_VALUE = value.length
+      const buffer = new ArrayBuffer(BYTES_PER_VALUE)
+      const view = new Uint8Array(buffer)
+      for (let i = 0; i < BYTES_PER_VALUE; i++) {
+        view[i] = value[i].charCodeAt();
+      }
+
+      this.obj[key] = view
+      this.i++;
+    },
+    get: function (key) {
+      if (typeof this.obj[key] === 'undefined') return;
+
+
+      let str = '';
+      for (let i = 0; i < this.obj[key].length; i++) {
+        str += String.fromCharCode(this.obj[key][i])
+      }
+      return str;
+    }
+  }
+}
+
+const jackBlack = Structure([['name', 'utf16', 10], ['lastName', 'utf16', 10], ['age', 'u16']])
+
+jackBlack.set('name', 'Jack');
+jackBlack.set('lastName', 'Black');
+jackBlack.set('age', 'Helloworld');
+console.log(jackBlack.get('name'))
+console.log(jackBlack.get('lastName'))
